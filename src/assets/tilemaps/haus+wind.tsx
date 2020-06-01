46ce962af2afed0ccc3a4fc43f738cd8442b045c
Sebastian Waschnick <sebastian.waschnick@now-services.de>
2020-06-01 08:50:14 +0200

    WIP

--- src/assets/tilemaps/haus+wind.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="haus+wind" tilewidth="32" tileheight="32" tilecount="702" columns="27">
- <image source="haus+wind.png" width="895" height="860"/>
-</tileset>
